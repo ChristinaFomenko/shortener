8b2f5c4d9f208304df92e77053dd685d752a127e
Christina Fomenko <kris.inferno@gmail.com>
2022-06-16 21:12:17 +0300

    fix

--- configs/config.go
+++ configs/config.go
@@ -20,5 +20,5 @@ package configs
 
 type Config struct {
 	ServerAddress string `env:"SERVER_ADDRESS" envDefault:"http://localhost:8080"`
-	BaseUrl       string `env:"BASE_URL" envDefault:"http://localhost:8080"`
+	BaseURL       string `env:"BASE_URL" envDefault:"http://localhost:8080"`
 }
