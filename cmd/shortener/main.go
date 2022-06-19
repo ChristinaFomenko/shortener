83a98a02261e99e4cbd4ea4334889f7da1ecfc50
Christina Fomenko <kris.inferno@gmail.com>
2022-06-19 18:59:16 +0300

    8-increment

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -7,6 +7,7 @@ import (
 	repositoryURL "github.com/ChristinaFomenko/shortener/internal/app/repository/urls"
 	serviceURL "github.com/ChristinaFomenko/shortener/internal/app/service/urls"
 	"github.com/ChristinaFomenko/shortener/internal/handlers"
+	"github.com/ChristinaFomenko/shortener/internal/middlewares"
 	"github.com/caarlos0/env"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
@@ -47,6 +48,8 @@ func main() {
 	router.Use(middleware.Logger)
 	router.Use(middleware.Recoverer)
 	router.Use(middleware.URLFormat)
+	router.Use(middlewares.Compressing)
+	router.Use(middlewares.Decompressing)
 
 	//router.Route("/", func(r chi.Router) {
 	router.Post("/", handlers.New(service).Shorten)
