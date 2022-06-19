cedfb18d2bc0bf4b873944ff51f88fc02e7457b5
Christina Fomenko <kris.inferno@gmail.com>
2022-06-19 19:04:49 +0300

    7-increment

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,46 +1,25 @@
 package main
 
 import (
-	"flag"
 	"github.com/ChristinaFomenko/shortener/configs"
 	"github.com/ChristinaFomenko/shortener/internal/app/generator"
 	repositoryURL "github.com/ChristinaFomenko/shortener/internal/app/repository/urls"
 	serviceURL "github.com/ChristinaFomenko/shortener/internal/app/service/urls"
 	"github.com/ChristinaFomenko/shortener/internal/handlers"
 	"github.com/ChristinaFomenko/shortener/internal/middlewares"
-	"github.com/caarlos0/env"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	log "github.com/sirupsen/logrus"
 	"net/http"
-	"net/url"
 )
 
-var Cfg configs.Config
-
-type serviceParams struct {
-	ServerAddress   *int
-	BaseURL         *string
-	FileStoragePath *string
-}
-
 func main() {
-	serviceParamsObj := &serviceParams{}
-
-	serviceParamsObj.ServerAddress = flag.Int("a", 8080, "port")
-	//serviceParamsObj.BaseURL = flag.String("b", "http://localhost:8080", "base url")
-	//serviceParamsObj.FileStoragePath = flag.String("f", "http://localhost:8080", "file path")
-	flag.Parse()
-
-	if err := env.Parse(&Cfg); err != nil {
-		log.Fatal("failed parse configs:", err)
-	}
 	// Repositories
 	repository := repositoryURL.Storage(configs.FileStoragePath())
 
 	// Services
 	helper := generator.NewGenerator()
-	service := serviceURL.NewService(repository, helper, Cfg.BaseURL)
+	service := serviceURL.NewService(repository, helper, configs.BaseURL())
 
 	// Route
 	router := chi.NewRouter()
@@ -57,12 +36,7 @@ func main() {
 	router.Post("/api/shorten", handlers.New(service).APIJSONShorten)
 	//})
 
-	//address := configs.ServerAddress()
-	//log.WithField("address", address).Info("server starts")
-	u, err := url.Parse(Cfg.BaseURL)
-	if err != nil {
-		log.Error(err)
-	}
-	log.WithField("address", *serviceParamsObj.ServerAddress).Info("server starts")
-	log.Fatal(http.ListenAndServe(u.Host, router), nil)
+	address := configs.ServerAddress()
+	log.WithField("address", address).Info("server starts")
+	log.Fatal(http.ListenAndServe(address, router), nil)
 }
--- configs/config.go
+++ configs/config.go
@@ -1,12 +1,68 @@
 package configs
 
-import "os"
+import (
+	"flag"
+	"os"
+)
 
-type Config struct {
-	ServerAddress string `env:"SERVER_ADDRESS" envDefault:"http://localhost:8080"`
-	BaseURL       string `env:"BASE_URL" envDefault:"http://localhost:8080"`
+func init() {
+	cfg.serverAddress = serverAddress()
+	cfg.baseURL = baseURL()
+	cfg.fileStoragePath = fileStoragePath()
+
+	flag.Parse()
+}
+
+var cfg struct {
+	serverAddress   *string
+	baseURL         *string
+	fileStoragePath *string
+}
+
+func ServerAddress() string {
+	if cfg.serverAddress == nil {
+		panic("server address not specified")
+	}
+
+	return *cfg.serverAddress
+}
+
+func BaseURL() string {
+	if cfg.baseURL == nil {
+		panic("base url not specified")
+	}
+
+	return *cfg.baseURL
 }
 
 func FileStoragePath() string {
-	return os.Getenv("FILE_STORAGE_PATH")
+	if cfg.fileStoragePath == nil {
+		panic("file storage path not specified")
+	}
+
+	return *cfg.fileStoragePath
+}
+
+func serverAddress() *string {
+	address := os.Getenv("SERVER_ADDRESS")
+	if address == "" {
+		address = ":8080"
+	}
+
+	return flag.String("a", address, "server address")
+}
+
+func baseURL() *string {
+	url := os.Getenv("BASE_URL")
+	if url == "" {
+		url = "http://localhost:8080"
+	}
+
+	return flag.String("b", url, "base url")
+}
+
+func fileStoragePath() *string {
+	path := os.Getenv("FILE_STORAGE_PATH")
+
+	return flag.String("f", path, "file storage path")
 }
