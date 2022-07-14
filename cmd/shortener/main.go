34b9146fce6ef03989eacb4b1694ff272e50e8c7
Christina Fomenko <kris.inferno@gmail.com>
2022-07-14 15:56:47 +0300

    fix ping

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -30,6 +30,9 @@ func main() {
 	if err != nil {
 		log.Fatalf("failed to create a storage %v", err)
 	}
+	defer func(repository repositoryURL.Repo) {
+		_ = repository.Close()
+	}(repository)
 
 	// Services
 	helper := generator.NewGenerator()
