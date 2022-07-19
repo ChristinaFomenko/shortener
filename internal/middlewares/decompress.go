82124e702db44d47f2272e17f8c0a383ebf7d1cf
Christina Fomenko <kris.inferno@gmail.com>
2022-07-19 14:54:58 +0300

    fix

--- internal/middlewares/decompress.go
+++ internal/middlewares/decompress.go
@@ -4,32 +4,36 @@ import (
 	"bytes"
 	"compress/gzip"
 	"io"
+	"io/ioutil"
 	"net/http"
-	"strings"
 )
 
 func Decompressing(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if !strings.Contains(r.Header.Get("Content-Encoding"), "gzip") {
-			next.ServeHTTP(w, r)
-			return
+		var reader io.Reader
+
+		if r.Header.Get(`Content-Encoding`) == `gzip` {
+			gz, err := gzip.NewReader(r.Body)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+
+			reader = gz
+			defer func(gz *gzip.Reader) {
+				_ = gz.Close()
+			}(gz)
+		} else {
+			reader = r.Body
 		}
 
-		gz, err := gzip.NewReader(r.Body)
+		body, err := io.ReadAll(reader)
 		if err != nil {
-			http.Error(w, err.Error(), http.StatusBadRequest)
+			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
 
-		var b bytes.Buffer
-		if _, err = b.ReadFrom(gz); err != nil {
-			http.Error(w, err.Error(), http.StatusBadRequest)
-			return
-		}
-
-		nr := io.NopCloser(bytes.NewReader(b.Bytes()))
-		rb, _ := http.NewRequest(r.Method, r.RequestURI, nr)
-		_ = r.Body.Close()
-		next.ServeHTTP(w, rb)
+		r.Body = ioutil.NopCloser(bytes.NewReader(body))
+		next.ServeHTTP(w, r)
 	})
 }
